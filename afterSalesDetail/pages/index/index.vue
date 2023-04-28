7b0e4af566762c4e718346e43ffbee96bdf8bf72
gmt <1765736910@qq.com>
2023-04-28 16:19:28 +0800

    售后原因取值字段修改

--- afterSalesDetail/pages/index/index.vue
+++ afterSalesDetail/pages/index/index.vue
@@ -52,7 +52,7 @@
 								</view>
 							</view>
 							<view class="info">
-								售后原因：{{dingdanGood.aftersaleReason}}
+								售后原因：{{afterSale.reason}}
 							</view>
 						</view>
 					</view>
